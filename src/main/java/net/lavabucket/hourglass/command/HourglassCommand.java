2d159c28714b577849c7d0a53188eeeba7f2b506
DuckyCrayfish <duckycrayfish@gmail.com>
2021-10-07 03:07:25 -0500

    Add allowDaySleep option to config command

--- src/main/java/net/lavabucket/hourglass/command/HourglassCommand.java
+++ src/main/java/net/lavabucket/hourglass/command/HourglassCommand.java
@@ -67,6 +67,7 @@ public class HourglassCommand {
                 .register(SERVER_CONFIG.sleepSpeedAll, DoubleArgumentType.doubleArg(-1, 24000))
                 .register(SERVER_CONFIG.clearWeatherOnWake)
                 .register(SERVER_CONFIG.displayBedClock)
+                .register(SERVER_CONFIG.allowDaySleep)
                 .register(SERVER_CONFIG.weatherEffect, EffectCondition.class)
                 .register(SERVER_CONFIG.randomTickEffect, EffectCondition.class)
                 .register(SERVER_CONFIG.baseRandomTickSpeed, IntegerArgumentType.integer(0))
