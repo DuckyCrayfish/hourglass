75d8c12c88039e485709f7a4efc2f9609ec7f3b3
DuckyCrayfish <duckycrayfish@gmail.com>
2021-10-10 08:22:58 -0500

    Add private constructor to utility class

--- src/main/java/net/lavabucket/hourglass/utils/MathUtils.java
+++ src/main/java/net/lavabucket/hourglass/utils/MathUtils.java
@@ -27,6 +27,9 @@ package net.lavabucket.hourglass.utils;
  */
 public final class MathUtils {
 
+    // Private constructor to prevent instantiation.
+    private MathUtils() {}
+
     /**
      * Returns a value from the range of numbers between {@code d0} and {@code d1}. The value's
      * position in this range is proportional to the argument {@code percent} as a fraction.
